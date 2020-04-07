18fe2662e8cb946cd6be9090a6e0511fa9291a05
Devil-404 <53328277+Devil-404@users.noreply.github.com>
2020-04-07 15:50:49 +0800

    Add files via upload

---
+++ Crack/Crack/Common.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Crack
+{
+    public class Common
+    {
+        /// <summary>
+        /// 定义ide下拉框的值
+        /// </summary>
+        public static Dictionary<int, string> IdeName { get; } = new Dictionary<int, string>
+        {
+            {0, "IntelliJIdea" },
+            {1, "PhpStorm" },
+            {2, "WebStorm" },
+            {3, "PyCharm" },
+            {4, "Clion" }
+        };
+
+        /// <summary>
+        /// ide下载地址，必须与ideName保持一致
+        /// </summary>
+        public static Dictionary<int, string> IdeUrl { get; } = new Dictionary<int, string>
+        {
+            {0, "https://download.jetbrains.8686c.com/idea/ideaIU-2019.3.4.exe" },
+            {1, "https://download.jetbrains.8686c.com/webide/PhpStorm-2019.3.4.exe" },
+            {2, "https://download.jetbrains.8686c.com/webstorm/WebStorm-2019.3.4.exe" },
+            {3, "https://download.jetbrains.8686c.com/python/pycharm-professional-2019.3.4.exe" },
+            {4, "https://download.jetbrains.8686c.com/cpp/CLion-2019.3.5.exe" }
+        };
+
+        /// <summary>
+        /// 下载是否停止的状态
+        /// </summary>
+        public static bool StopDownLoad { get; set; } = true;
+        public static int DownloadNum { get; set; } = 0;
+
+        #region  判断是否联网
+        [DllImport("winInet.dll")]
+        private static extern bool InternetGetConnectedState(
+        ref int dwFlag,
+        int dwReserved
+        );
+
+
+        /// <summary>
+        /// 判断是否联网
+        /// </summary>
+        /// <returns>true 是联网，false 是断网</returns>
+        public static bool Networking()
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingOptions poptions = new PingOptions();
+                poptions.DontFragment = true;
+                string data = string.Empty;
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                System.Int32 dwFlag = new int();
+                if (!InternetGetConnectedState(ref dwFlag, 0))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("判断是否联网！" + ex.Message);
+            }
+        }
+
+        public static void DialogNetworking(string Prompt)
+        {
+            MessageBox.Show(Prompt, "系统消息");
+        }
+        #endregion
+    }
+}
---
+++ Crack/Crack/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Crack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.userControlCrack1 = new Crack.UserControlCrack();
+            this.userControldownload1 = new Crack.UserControldownload();
+            this.SuspendLayout();
+            // 
+            // userControlCrack1
+            // 
+            this.userControlCrack1.BackColor = System.Drawing.Color.Transparent;
+            this.userControlCrack1.Location = new System.Drawing.Point(13, 12);
+            this.userControlCrack1.Name = "userControlCrack1";
+            this.userControlCrack1.SfoPath = null;
+            this.userControlCrack1.Size = new System.Drawing.Size(372, 204);
+            this.userControlCrack1.TabIndex = 2;
+            this.userControlCrack1.UserPath = "C:\\Users\\Devil\\.IntelliJIdea2019.3\\config\\";
+            // 
+            // userControldownload1
+            // 
+            this.userControldownload1.BackColor = System.Drawing.Color.Transparent;
+            this.userControldownload1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.userControldownload1.GbText = null;
+            this.userControldownload1.Location = new System.Drawing.Point(12, 213);
+            this.userControldownload1.Name = "userControldownload1";
+            this.userControldownload1.Size = new System.Drawing.Size(372, 265);
+            this.userControldownload1.TabIndex = 1;
+            this.userControldownload1.Uurl = "https://download.jetbrains.8686c.com/idea/ideaIU-2019.3.3.exe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(397, 496);
+            this.Controls.Add(this.userControlCrack1);
+            this.Controls.Add(this.userControldownload1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crack";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private UserControldownload userControldownload1;
+        private UserControlCrack userControlCrack1;
+    }
+}
+
