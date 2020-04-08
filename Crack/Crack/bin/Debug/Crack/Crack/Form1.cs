90a58b68cf1b8970c9fcb557a57242d59c969a3b
Devil-404 <53328277+Devil-404@users.noreply.github.com>
2020-04-08 22:28:42 +0800

    Add files via upload

---
+++ Crack/Crack/bin/Debug/Crack/Crack/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Crack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //userControldownload1.BackColor= Color.FromArgb(90, 173, 173, 173);
+            //userControlCrack1.BackColor= Color.FromArgb(90, 173, 173, 173);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+        }
+    }
+}
